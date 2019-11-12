558de8d34e28494750b85934cfc2a4b376c5f21b
Jaromír Stanko <jaromir.stanko@aerobtec.com>
2019-11-12 10:07:56 +0100

    SPI communication and display

---
+++ Inc/spi.h
@@ -0,0 +1,71 @@
+/**
+  ******************************************************************************
+  * File Name          : SPI.h
+  * Description        : This file provides code for the configuration
+  *                      of the SPI instances.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __spi_H
+#define __spi_H
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+void MX_SPI1_Init(void);
+
+unsigned char readWriteSPI1(unsigned char txData);
+void initCS_Pin(void);
+void cs_set(void);
+void cs_reset(void);
+
+void initCD_Pin(void);
+void cd_set(void);
+void cd_reset(void);
+
+void initRES_Pin(void);
+void res_set(void);
+void res_reset(void);
+
+void device_Unselect(void);
+void device_Select(void);
+/* USER CODE BEGIN Prototypes */
+
+/* USER CODE END Prototypes */
+
+#ifdef __cplusplus
+}
+#endif
+#endif /*__ spi_H */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ README.md
@@ -0,0 +1,7 @@
+# Náplň cvičenia
+- zoznámenie sa s SPI perifériou
+- komunikácia s displejom ILI9163
+
+
+# Konfigurácia SPI
+
